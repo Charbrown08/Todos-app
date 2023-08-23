8d18c6a50a71cefc78f172c8798ea14e4843dd91
Charbrown08 <cartam15@hotmail.com>
2023-08-23 16:21:25 -0500

    Listenimg events ON

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,16 @@
 import "./TodoSearch.css";
 function TodoSearch() {
-  return <input placeholder="Cortar Cebolla" className="TodoSearch" />;
+  return (
+    <input
+      placeholder="Sacar la basura todos los lunes "
+      className="TodoSearch"
+      onChange={(event) => {
+        console.log("Escribistes en el TodoSearch");
+        console.log(event);
+        console.log(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
