f2743bf45066d90a7234097c76467235bffd7b12
Charbrown08 <cartam15@hotmail.com>
2023-08-28 16:45:08 -0500

    add contexts

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,8 +1,12 @@
+import React from "react";
 import "./TodoCounter.css";
-function TodoCounter({ completed, total }) {
+import { TodoContext } from "../TodoContext";
+function TodoCounter() {
+  const { completedTodos, totalTodos } = React.useContext(TodoContext);
   return (
     <h1 className="TodoCounter">
-      Has Completado <span>{completed}</span> de <span>{total}</span> tareas
+      Has Completado <span>{completedTodos}</span> de <span>{totalTodos}</span>{" "}
+      tareas
     </h1>
   );
 }
